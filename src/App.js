3070c9071bd3b875e64ac9a2a4d4534e2f425ac0
WuJianRong <2270645424@qq.com>
2023-05-09 23:22:24 +0800

    路由懒加载完成

--- src/App.js
+++ src/App.js
@@ -4,34 +4,53 @@ import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom";
 import history from "./utils/history";
 
 import { AnthComponent } from "@comp/AuthComponent";
-import LayOut from "@/pages/Layout";
-import Login from "@/pages/Login";
-import Publish from "./pages/Publish";
-import Home from "./pages/Home";
-import Article from "./pages/Article";
-
+// import LayOut from "@/pages/Layout";
+// import Login from "@/pages/Login";
+// import Publish from "./pages/Publish";
+// import Home from "./pages/Home";
+// import Article from "./pages/Article";
 import "./App.css";
+// 导入路由懒加载必要组件
+import { lazy, Suspense } from "react";
+// 按需导入路由组件
+const Login = lazy(() => import("./pages/Login"));
+const LayOut = lazy(() => import("./pages/LayOut"));
+const Article = lazy(() => import("./pages/Article"));
+const Home = lazy(() => import("./pages/Home"));
+const Publish = lazy(() => import("./pages/Publish"));
 
 function App() {
   return (
     // 路由配置
     <HistoryRouter history={history}>
       <div className="App">
-        <Routes>
-          <Route path="/Login" element={<Login></Login>}></Route>
-          <Route
-            path="/"
-            element={
-              <AnthComponent>
-                <LayOut></LayOut>
-              </AnthComponent>
-            }
-          >
-            <Route index element={<Home></Home>}></Route>
-            <Route path="article" element={<Article></Article>}></Route>
-            <Route path="publish" element={<Publish></Publish>}></Route>
-          </Route>
-        </Routes>
+        {/* 路由懒加载  */}
+        <Suspense
+          fallback={
+            <div style={{ textAlign: "center", marginTop: 200 }}>
+              loading...
+            </div>
+          }
+        >
+          <Routes>
+            {/* 不需要鉴权的组件 */}
+            <Route path="/Login" element={<Login></Login>}></Route>
+            {/* 需要鉴权的组件 */}
+            <Route
+              path="/"
+              element={
+                <AnthComponent>
+                  <LayOut></LayOut>
+                </AnthComponent>
+              }
+            >
+              {/* 二级路由默认页面 */}
+              <Route index element={<Home></Home>}></Route>
+              <Route path="article" element={<Article></Article>}></Route>
+              <Route path="publish" element={<Publish></Publish>}></Route>
+            </Route>
+          </Routes>
+        </Suspense>
       </div>
     </HistoryRouter>
   );
